dc9dd24f49e22a4b683f35816683d9ae92abd02e
unknown <weryogesh54@gmail.com>
2020-09-12 11:02:13 +0530

    Updates

---
+++ precache-manifest.3fe8c5f195b44385cc51695f13773a6d.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "739b940a1c5f622c56d2417cb41cd467",
+    "url": "/yogeshwer77/BusinessSite.git/index.html"
+  },
+  {
+    "revision": "3abced7902f01d18f3ef",
+    "url": "/yogeshwer77/BusinessSite.git/static/css/2.ea6b7726.chunk.css"
+  },
+  {
+    "revision": "03a01adef99647b3efb2",
+    "url": "/yogeshwer77/BusinessSite.git/static/css/main.98ca4d96.chunk.css"
+  },
+  {
+    "revision": "3abced7902f01d18f3ef",
+    "url": "/yogeshwer77/BusinessSite.git/static/js/2.c97ce742.chunk.js"
+  },
+  {
+    "revision": "361388382a7cba0b7cd58ae9b81faa0e",
+    "url": "/yogeshwer77/BusinessSite.git/static/js/2.c97ce742.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "03a01adef99647b3efb2",
+    "url": "/yogeshwer77/BusinessSite.git/static/js/main.9e2f6917.chunk.js"
+  },
+  {
+    "revision": "122bf9be0481c2f78bed",
+    "url": "/yogeshwer77/BusinessSite.git/static/js/runtime-main.8398b413.js"
+  },
+  {
+    "revision": "72ae227f595a0aa064b37a68fd2b6564",
+    "url": "/yogeshwer77/BusinessSite.git/static/media/Srv1.72ae227f.jpg"
+  },
+  {
+    "revision": "780cf33091178f2712c73c7ceafbb0bb",
+    "url": "/yogeshwer77/BusinessSite.git/static/media/Srv2.780cf330.jpg"
+  },
+  {
+    "revision": "7af82ce7ed5fdbbd799e8929d6ce1cf3",
+    "url": "/yogeshwer77/BusinessSite.git/static/media/Srv3.7af82ce7.jpg"
+  },
+  {
+    "revision": "4a3ae2f439acc33050f33d3103bf9cfd",
+    "url": "/yogeshwer77/BusinessSite.git/static/media/Srv4.4a3ae2f4.jpg"
+  },
+  {
+    "revision": "6465f6bea73898f01545a873d42d9c2e",
+    "url": "/yogeshwer77/BusinessSite.git/static/media/Srv5.6465f6be.jpg"
+  },
+  {
+    "revision": "0e82f86a715e816f516cd97a37d094b3",
+    "url": "/yogeshwer77/BusinessSite.git/static/media/Srv6.0e82f86a.jpg"
+  },
+  {
+    "revision": "0ef7a9df500a058570967746572ff576",
+    "url": "/yogeshwer77/BusinessSite.git/static/media/img1.0ef7a9df.svg"
+  },
+  {
+    "revision": "5312ef847c52086bcfb9e4f344c072f1",
+    "url": "/yogeshwer77/BusinessSite.git/static/media/img1svg.5312ef84.svg"
+  }
+]);
